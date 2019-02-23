a9efc7a6637a5707a6f18c5f8502c8bc49dd6b3a
Jérémy Buget <jbuget@gmail.com>
2019-02-23 19:41:48 +0100

    Change Sentry project ID

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@ import config from './config/environment';
 import * as Sentry from '@sentry/browser'
 
 Sentry.init({
-  dsn: 'https://c09ad627fd494dcea5a2d67609f93b0a@sentry.io/1400959',
+  dsn: 'https://f7589b9ed7c24fc0a6c5b20ea5ca3885@sentry.io/1401066',
   integrations: [new Sentry.Integrations.Ember()],
   release: config.APP.version,
   environment: config.environment,
