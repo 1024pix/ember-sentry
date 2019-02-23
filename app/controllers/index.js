65e8007e6b21ebcb886c47e360b2fa60466607ea
Jérémy Buget <jbuget@gmail.com>
2019-02-23 15:30:21 +0100

    Add button that send events to Sentry

---
+++ app/controllers/index.js
@@ -0,0 +1,16 @@
+import Controller from '@ember/controller';
+import * as Sentry from '@sentry/browser'
+
+export default Controller.extend({
+
+actions: {
+
+  sendInfoEvent() {
+    return Sentry.captureMessage('Message captured!');
+  },
+
+  sendErrorEvent() {
+    throw new Error("Throwed error");
+  }
+}
+});
