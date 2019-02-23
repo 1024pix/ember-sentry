8c1cdc3dbaa939637cffda565a53319759cbd395
Jérémy Buget <jbuget@gmail.com>
2019-02-23 13:28:57 +0100

    Add Sentry dependency & configuration

--- app/app.js
+++ app/app.js
@@ -3,6 +3,16 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
+/* Sentry integration */
+/* --- Begin --- */
+import * as Sentry from '@sentry/browser'
+
+Sentry.init({
+  dsn: 'https://c09ad627fd494dcea5a2d67609f93b0a@sentry.io/1400959',
+  integrations: [new Sentry.Integrations.Ember()]
+});
+/* --- end --- */
+
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
