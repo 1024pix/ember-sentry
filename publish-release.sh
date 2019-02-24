0a8997ce0dacd79829e45849364ecd205887b505
Jérémy Buget <jbuget@gmail.com>
2019-02-24 14:27:21 +0100

    Add blank line in publish-release script

--- publish-release.sh
+++ publish-release.sh
@@ -12,20 +12,20 @@ SENTRY_VERSION="ember-sentry-${PACKAGE_VERSION}"
 
 echo "Prepare release on Sentry"
 sentry-cli releases -o pix -p ember-sentry new ${SENTRY_VERSION} --finalize
-echo "Release ${SENTRY_VERSION} successfully defined ✅"
+echo "Release ${SENTRY_VERSION} successfully defined ✅\n"
 
 echo "Push version bumping in package.json on branch master"
 git push origin master
-echo "Commit pushed on GitHub ✅"
+echo "Commit pushed on GitHub ✅\n"
 
 echo "Push code on Scalingo"
 git push scalingo master
-echo "Commit pushed on Scalingo ✅"
+echo "Commit pushed on Scalingo ✅\n"
 
 echo "Publish tag"
 git push --tags
-echo "Tag published ✅"
+echo "Tag published ✅\n"
 
 echo "Tell Sentry that release is well deployed on environment production"
 sentry-cli releases -o pix deploys ${SENTRY_VERSION} new -e production
-echo "Sentry notification sent ✅"
+echo "Sentry notification sent ✅\n"
